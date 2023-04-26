b749d7e79879610a914d9db7f0374b69a63d6e66
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-04-27 02:40:08 +0600

    update code

---
+++ 01_output.js
@@ -0,0 +1,13 @@
+// ******** working in console ******** //
+console.log("Hello World!")
+console.log(123)
+
+
+
+
+// ******** working in browser ******** //
+
+// alert("Hello World!")
+// document.write("Hello World!")
+
+
---
+++ 03_dataTypes.js
@@ -0,0 +1,28 @@
+// ******** string ******** //
+console.log("Mostafizur Rahman Fuad")
+console.log('Mostafizur Rahman Fuad')
+console.log(`Mostafizur Rahman Fuad`)
+
+
+
+// ******** number ******** //
+console.log(524)
+console.log(524.45)
+
+
+
+// ******** boolean ******** //
+console.log(true)
+console.log(false)
+
+
+
+// ******** object ******** //
+// it's a large chapter. We will discuss about object in the next chapter.
+
+
+
+// ******** typeof() function : এর মধ্যে যেকোনো ডেটা দিলে সেই ডেটার টাইপ বলে দিবে ******** //
+console.log(typeof("Mostafizur Rahman Fuad"))
+console.log(typeof(255))
+console.log(typeof(true))
